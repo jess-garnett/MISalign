85b930d571360a578bbd1e3ed2c13a24945a9d87
Jess Garnett <jgarnett@umd.edu>
2023-09-27 12:16:04 -0400

    Setting up sphinx for documentation

---
+++ doc/conf.py
@@ -0,0 +1,33 @@
+# Configuration file for the Sphinx documentation builder.
+#
+# For the full list of built-in configuration values, see the documentation:
+# https://www.sphinx-doc.org/en/master/usage/configuration.html
+
+# -- Project information -----------------------------------------------------
+# https://www.sphinx-doc.org/en/master/usage/configuration.html#project-information
+
+import os
+import sys
+sys.path.insert(0, os.path.abspath(".."))  # Source code dir relative to this file
+
+project = 'MISalign'
+copyright = '2023, Jess Garnett'
+author = 'Jess Garnett'
+release = '0.1'
+
+# -- General configuration ---------------------------------------------------
+# https://www.sphinx-doc.org/en/master/usage/configuration.html#general-configuration
+
+extensions = ['sphinx.ext.autodoc', 'sphinx.ext.autosummary']
+autosummary_generate = True
+autodoc_mock_imports = ['PIL']
+templates_path = ['_templates']
+exclude_patterns = ['_build', 'Thumbs.db', '.DS_Store']
+
+
+
+# -- Options for HTML output -------------------------------------------------
+# https://www.sphinx-doc.org/en/master/usage/configuration.html#options-for-html-output
+
+html_theme = 'alabaster'
+html_static_path = ['_static']
---
+++ doc/index.rst
@@ -0,0 +1,20 @@
+.. MISalign documentation master file, created by
+   sphinx-quickstart on Wed Sep 27 10:52:00 2023.
+   You can adapt this file completely to your liking, but it should at least
+   contain the root `toctree` directive.
+
+Welcome to MISalign's documentation!
+====================================
+
+.. autosummary::
+   :toctree: _autosummary
+   :recursive:
+
+   MISalign
+
+Indices and tables
+==================
+
+* :ref:`genindex`
+* :ref:`modindex`
+* :ref:`search`
