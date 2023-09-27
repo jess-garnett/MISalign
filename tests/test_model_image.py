39c4c3d48f14fed1ca8f42bf381de90867dd6a99
Jess Garnett <jgarnett@umd.edu>
2023-09-27 17:41:31 -0400

    got tests for dfe and img rectangular passing.

--- tests/test_model_image.py
+++ tests/test_model_image.py
@@ -10,10 +10,10 @@ class TestImage():
     def test_image_dfe_rect(self):
         test_img_a01=r"example\data\set_a\a_myimages01.jpg"
         test_image=Image(test_img_a01)
-        test_dfe_arr_fp=r"..\example\expected_result\set_a\dfe_rectangular.npy"
-        assert test_image.dfe_arr()==np.load(test_dfe_arr_fp)
+        test_dfe_arr_fp=r"example\expected_result\set_a\dfe_rectangular.npy"
+        assert np.all(test_image.dfe_arr()==np.load(test_dfe_arr_fp))
     def test_image_img_rect(self):
         test_img_a01=r"example\data\set_a\a_myimages01.jpg"
         test_image=Image(test_img_a01)
-        test_img_arr_fp=r"..\example\expected_result\set_a\img_a01.npy"
-        assert test_image.img_arr()==np.load(test_img_arr_fp)
+        test_img_arr_fp=r"example\expected_result\set_a\img_a01.npy"
+        assert np.all(test_image.img_arr()==np.load(test_img_arr_fp))
