0292ff45f5d7ab9b32371b7566235145d52b0a49
Jess Garnett <jgarnett@umd.edu>
2024-05-03 15:08:59 -0400

    adds find_image_size function.

--- MISalign/canvas/canvas_render.py
+++ MISalign/canvas/canvas_render.py
@@ -1,13 +1,17 @@
 """ Canvas Render
 - Renders combined images.
 """
-
+from PIL import Image as PILImage
 # Rectangular Render
     # Uses solution from rectangular_solve
 
 ## Find Extents
     ### Image Sizes
-
+def find_image_size(image_filepaths:dict) -> dict:
+    """ Gets image size from a dictionary of image filepaths.
+    - Takes a dictionary: {image_name:image_filepath}
+    - Returns a dictionary of image sizes: {image_name:(width,height)}"""
+    return {img_name:PILImage.open(img_fp).size for img_name,img_fp in image_filepaths.items()}
     ### Generate Points
 
     ### Resolve Extents
