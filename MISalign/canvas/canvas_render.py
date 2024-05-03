49e0d1ecbb91c6c8d599c9bc4cdb5793c449ccf1
Jess Garnett <jgarnett@umd.edu>
2024-05-03 14:52:01 -0400

    outlines canvas render module

--- MISalign/canvas/canvas_render.py
+++ MISalign/canvas/canvas_render.py
@@ -1,3 +1,27 @@
 """ Canvas Render
 - Renders combined images.
 """
+
+# Rectangular Render
+    # Uses solution from rectangular_solve
+
+## Find Extents
+    ### Image Sizes
+
+    ### Generate Points
+
+    ### Resolve Extents
+
+## Place In Canvas
+
+## Rectangular Unblended Render
+
+## Rectangular Blended Render
+
+    ### Distance-From-Edge Weight
+
+    ### Flat Weight
+
+    ### Normalization Matrix Building
+    
+    ### Summation Blending
