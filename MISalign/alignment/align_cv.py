bc8f9b1ac7d8b2bddb12dfd5311b042e9e6a987b
Jess Garnett <jgarnett@umd.edu>
2024-12-13 11:46:51 -0500

    preliminary estimate translation validation

--- MISalign/alignment/align_cv.py
+++ MISalign/alignment/align_cv.py
@@ -104,7 +104,8 @@ def estimate_translation(
         limit_affine_rotation=0.01, #maximum affine rotation without correction i.e. -0.01rad to +0.01rad rotations are acceptable.
         limit_affine_scale=0.01, #maximum affine scale without correction i.e. 0.99 scale to 1.01 scale are acceptable.
         limit_minimum_points=5, #minimum number of matched points for succesful translation estimate.
-        limit_inlier_consistency=0.5 #minimum fraction of matched points that must be consistent
+        limit_inlier_consistency=0.6, #minimum fraction of matched points that must be consistent.
+        limit_inlier_radius=10 # minimum L2 distance between points to be considered consistent.
     )->list[list,bool]:
     
     pts_1=np.array([orb1["kp"][match.queryIdx].pt for match in matches])
@@ -113,21 +114,37 @@ def estimate_translation(
 
     eAP2D_translationx=eAP2D_matrix[0,2]
     eAP2D_translationy=eAP2D_matrix[1,2]
-    # print(f"X,Y:{eAP2D_translationx:0.1f}, {eAP2D_translationy:0.1f}")
-    # print("Rotation & Scale Matrix: \n",eAP2D_matrix[:,:2])
     eAP2D_scalex=(eAP2D_matrix[0,0]**2+eAP2D_matrix[0,1]**2)**0.5
     eAP2D_scaley=(eAP2D_matrix[1,1]**2+eAP2D_matrix[1,0]**2)**0.5
-    # print(f"Scale X,Y: {eAP2D_scalex:0.3f}, {eAP2D_scaley:0.3f}")
     eAP2D_rotation=np.arctan(eAP2D_matrix[0,1]/eAP2D_matrix[1,1])
-    # print(f"Rotation(Rad/Deg): {eAP2D_rotation:0.3f} /{eAP2D_rotation*180/np.pi:0.1f}")
-    # print("Inliers:",np.sum(eAP2D[1]))
     eAP2D_inliers=[(tuple(pt1),tuple(pt2)) for pt1,pt2,in_out in zip(pts_1,pts_2,eAP2D_in_out) if in_out]
     eAP2D_inliers_count=len(eAP2D_inliers)
-    # for pt_from,pt_to,in_out in zip(pts_from,pts_to,eAP2D[1]):
-    #     if in_out:
-    #         print(f"X,Y:{pt_from[0]-pt_to[0]:0.1f}, {pt_from[1]-pt_to[1]:0.1f}")
-    #TODO verify based on limits
-    valid={"overall":True,"individual":[True for match in eAP2D_inliers]}
+    if abs(eAP2D_rotation)<limit_affine_rotation and abs(eAP2D_scalex-1)<limit_affine_scale:
+        # rotation is close to 0 and scale is close to 1 > Likely good translation fit.
+        if eAP2D_inliers_count>=limit_minimum_points:
+            # reasonable number of points have been found
+            overall=True
+            individual=[True]*eAP2D_inliers_count
+        else:
+            # too few points have been found
+            overall=False
+            individual=[True]*eAP2D_inliers_count
+    else:
+        # rotation or scale are outside limits.
+        inlier_translations=[(pt1[0]-pt2[0],pt1[1]-pt2[1]) for pt1,pt2 in eAP2D_inliers] # convert points pairs to translations
+        inter_inlier_distances=[[((inlier_trans[0]-other_trans[0])**2+(inlier_trans[1]-other_trans[1])**2)**0.5 for other_trans in inlier_translations]
+            for inlier_trans in inlier_translations] # find distance between all inlier-inlier distances
+        inlier_radius_limit=[[distance<limit_inlier_radius for distance in iid] for iid in inter_inlier_distances] # check i-i distances less than radius limit
+        inlier_consistency_limit=[sum(irl)>=limit_inlier_consistency*eAP2D_inliers_count for irl in inlier_radius_limit] # check radius limit counts for consistency
+        individual=inlier_consistency_limit
+        overall=sum(individual)>=limit_minimum_points # check reasonable number of points have been found
+        # print(inlier_translations)
+        # print(inter_inlier_distances)
+        # print(inlier_radius_limit)
+        # print(inlier_consistency_limit)
+        # print(individual)
+        # print(overall) #TODO add more warning/error reporting or optional passthrough
+    valid={"overall":overall,"individual":individual}
     return eAP2D_inliers, valid
 
 class AlignCVManager():
@@ -146,7 +163,7 @@ class AlignCVManager():
                 OrbM=self.OrbM,
                 ORB_parameters=ORB_parameters
                 )
-            estimated_translation_inliers, estimated_translation_validity = estimate_translation(matches,orb1,orb2)
+            estimated_translation_inliers, estimated_translation_validity = estimate_translation(matches,orb1,orb2) #TODO estimate_translation parameter passthrough
             alignment_data["alignment_data"][image_set]={
                 "matches":matches,
                 "estimated_translation_inliers":estimated_translation_inliers,
@@ -156,7 +173,6 @@ class AlignCVManager():
         return len(self.alignments)-1
     def get_alignment(self,alignment_id):
         return self.alignments[alignment_id]
-    #TODO valid results to mis
     #TODO merging valid results
     def update_mis(self,
             mis_project:MisFile,
@@ -173,4 +189,4 @@ class AlignCVManager():
                 mc[0],
                 mc[1],
                 'p',
-                [([int(x) for x in pt1],[int(x) for x in pt2]) for (pt1,pt2),individual_valid in zip(mc_alignment_result['estimated_translation_inliers'],mc_alignment_result["estimated_translation_validity"]["individual"])] ))
+                [([int(x) for x in pt1],[int(x) for x in pt2]) for (pt1,pt2),individual_valid in zip(mc_alignment_result['estimated_translation_inliers'],mc_alignment_result["estimated_translation_validity"]["individual"]) if individual_valid] ))
