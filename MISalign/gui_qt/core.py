5033109d34c37c2858cbd87b7e57ae24daa69c29
Jess Garnett <71046223+jess-garnett@users.noreply.github.com>
2025-10-30 16:42:27 -0400

    Base of MVP structure for core module.

--- MISalign/gui_qt/core.py
+++ MISalign/gui_qt/core.py
@@ -1,2 +1,54 @@
 """Core module for MISalign GUI based on QT."""
 
+import pyqtgraph as pg
+from pyqtgraph.Qt.QtWidgets import QMainWindow
+from pyqtgraph.dockarea.DockArea import DockArea
+from pyqtgraph.dockarea.Dock import Dock
+
+class CoreModel():
+    """Model for Core of MISalign QT GUI"""
+    def __init__(self):
+        pass
+
+
+class CoreView():
+    """View for Core of MISalign QT GUI"""
+    def __init__(self):
+        self.window:QMainWindow = QMainWindow()
+        self.area:DockArea = DockArea()
+        self.window.setCentralWidget(self.area)
+        self.window.resize(1000,500)
+
+
+        self.dock_tabs={
+            "MIS File":Dock(name="MIS File"),
+            "Setup":Dock(name="Setup"),
+            "Alignment":Dock(name="Alignment"),
+            "Render":Dock(name="Render"),
+        }
+        self.area.addDock(dock=self.dock_tabs["MIS File"])
+        self.area.addDock(dock=self.dock_tabs["Setup"],
+            position='below',relativeTo=self.dock_tabs["MIS File"])
+        self.area.addDock(dock=self.dock_tabs["Alignment"],
+            position='below',relativeTo=self.dock_tabs["MIS File"])
+        self.area.addDock(dock=self.dock_tabs["Render"],
+            position='below',relativeTo=self.dock_tabs["MIS File"])
+
+class CorePresenter():
+    """Presenter for Core of MISalign QT GUI"""
+    def __init__(self,
+        model:CoreModel,
+        view:CoreView):
+
+        self.core_model=model
+        self.core_view=view
+
+        self.core_view.window.show()
+
+if __name__ == '__main__':
+    app = pg.mkQApp(name="MISalign QT GUI")
+    core_presenter=CorePresenter(
+        model=CoreModel(),
+        view=CoreView()
+        )
+    pg.exec()
