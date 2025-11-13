7004146c6115ffac9166b4b9414a205ee1a561f7
Jess Garnett <71046223+jess-garnett@users.noreply.github.com>
2025-11-13 17:12:38 -0500

    Begins interface development for plugin extension.

--- MISalign/model/relation.py
+++ MISalign/model/relation.py
@@ -9,6 +9,7 @@
 - Can generate a relation tree graphic/table/something showing relation chains
 """
 from statistics import mean
+from typing import Protocol
 
 class Relation():
     """Stores the relationship between two images."""
@@ -70,3 +71,88 @@ class Relation():
         elif self._relation=='p':
             data=self._points
         return [self.ref,self._relation,data]
+    
+
+
+
+class MISRelation(Protocol):
+    """Contains information relating an image pair."""
+    def __str__(self)->str:
+        """String Representation of the Relation."""
+        ...
+    def get_reference(self)->tuple[str,str]:
+        """Get the images names of the pair of images that are related."""
+        ...
+    def get_relation(self,relation_type):
+        """Get the relation between the images in the specified relation type."""
+        ...
+    def save_relation(self)->list:
+        """Get the save list of the relation."""
+        ...
+
+class MISRelationReference():
+    """Image pair that is related but no specific relation is known.
+    - `relation_type=None`"""
+    def __init__(self,image_a,image_b,*data):
+        self._reference=(image_a,image_b)
+        self._relation_type=None
+    def __str__(self)->str:
+        """String Representation of the Relation."""
+        return f"Image {self._reference[0]} is related to image {self._reference[1]}."
+    def get_reference(self)->tuple[str,str]:
+        """Get the images names of the pair of images that are related."""
+        return self._reference
+    def get_relation(self,relation_type):
+        """Get the relation between the images in the specified relation type."""
+        return None
+    def save_relation(self)->list:
+        """Get the save list of the relation."""
+        return [self._reference,self._relation_type,None]
+
+class MISRelationRectangular():
+    """Contains information relating an image pair in terms of (x,y) offset.
+    - `relation_type='r'`
+    - rectilinear relationship A(0,0)->B(0,0)"""
+    def __init__(self,image_a,image_b,*data):
+        self._reference=(image_a,image_b)
+        self._relation_type='r'
+        self._rect=data[0]
+
+    def __str__(self)->str:
+        """String Representation of the Relation."""
+        return f"Image {self._reference[0]} is related to image {self._reference[1]} by {self._rect}."
+    def get_reference(self)->tuple[str,str]:
+        """Get the images names of the pair of images that are related."""
+        return self._reference
+    def get_relation(self,relation_type):
+        """Get the relation between the images in the specified relation type."""
+        ... #TODO pickup here. Case switch/match?
+    def save_relation(self)->list:
+        """Get the save list of the relation."""
+        ...
+
+class MISRelationPoints():
+    """Contains information relating an image pair in terms of matching points.
+    - `relation_type='p'`
+    - point-based relation Ai->Bi"""
+    def __str__(self)->str:
+        """String Representation of the Relation."""
+        ...
+    def get_reference(self)->tuple[str,str]:
+        """Get the images names of the pair of images that are related."""
+        ...
+    def get_relation(self,relation_type):
+        """Get the relation between the images in the specified relation type."""
+        ...
+    def save_relation(self)->list:
+        """Get the save list of the relation."""
+        ...
+
+
+relation_types={
+    None:MISRelationReference,
+    'r':MISRelationRectangular,
+    'p':MISRelationPoints,
+}
+def setup_relation(image_pair,relation_type,relation_data)->MISRelation:
+    return relation_types[relation_type](image_pair,relation_data)
