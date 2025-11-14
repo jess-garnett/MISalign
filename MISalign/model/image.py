b148b49f3ebb607929ca09dfa80a548183bda1f4
Jess Garnett <71046223+jess-garnett@users.noreply.github.com>
2025-11-14 11:29:50 -0500

    implemented MISImageFile

--- MISalign/model/image.py
+++ MISalign/model/image.py
@@ -2,6 +2,7 @@ from PIL import Image as PILImage
 import numpy as np
 from os.path import split
 from typing import Protocol, runtime_checkable
+from pathlib import Path
 
 class Image():
     """Store image data and generates numpy arrays for image and distance from edge(DFE)
@@ -9,7 +10,7 @@ class Image():
     """
     def __init__(self,image_fp:str):
         self.image_fp=image_fp
-        self.name=split(image_fp)[1]
+        self.name=split(image_fp)[1].split
         PIL_image=PILImage.open(image_fp)
         self.image=PIL_image.convert("RGB")
         self.size=self.image.size
@@ -17,7 +18,7 @@ class Image():
         self._dfe=self._dfe_rectangular()
 
     def __str__(self):
-        return "Image '"+self.name+"' with shape:"+str(self._img.shape)
+        return "Image '"+self.name+"' with shape:"+str(self.size)
     
     def _dfe_rectangular(self) -> np.ndarray:
         np_size=(self.size[1],self.size[0]) #converts from (Max X, Max Y) to (#rows, #columns)
@@ -40,18 +41,48 @@ class Image():
         
 #TODO move dfe generation out of the image class.
     # It already is? render.py has it's own dfe/weight generation. this code doesn't get used at all?
+#TODO make render modules actually use the image array instead of just opening the image themselves.
 
 @runtime_checkable
 class MISImage(Protocol):
     """Access image data and information."""
     def __str__(self)->str:
         ...
-    def get_image_array(self)->np.ndarray:
+    def get_image_array(self,PIL_Mode:str)->np.ndarray:
+        """Get a nparray of the image."""
         ...
-    def get_image_size(self)->str:
+    def get_image_size(self)->tuple[int,int]:
+        """Get the size of the image."""
         ...
 
 class MISImageFile():
     """Access image data and information for an image file."""
+    def __init__(self,image_filepath:str|Path):
+        self.image_filepath=Path(image_filepath)
+        self.name=self.image_filepath.stem
+        self._PIL_mode=None
+    def __str__(self):
+        return "Image '"+self.name+"' with shape:"+str(self.get_image_size())
+    def get_image_array(self,PIL_mode:str="RGB")->np.ndarray:
+        """Get a nparray of the image."""
+        if self._PIL_mode==PIL_mode:
+            return self._array
+        else:
+            PIL_image=PILImage.open(self.image_filepath)
+            PIL_image=PIL_image.convert(PIL_mode)
+            self._PIL_mode=PIL_mode
+            self._array=np.asarray(PIL_image)
+            self._size=PIL_image.size
+            return self._array
+        #TODO option for not keeping the array in memory when working with very large objects.
+    def get_image_size(self)->tuple[int,int]:
+        """Get the size of the image."""
+        try: # if image has already been opened just get the size that was stored.
+            return self._size
+        except: # if image hasn't been opened then open it and grab the size.
+            self.get_image_array()
+            return self._size
+
+
 class MISImageHDF5():
     """Access image data and information from a HDF5."""
--- tests/test_model_image.py
+++ tests/test_model_image.py
@@ -1,19 +1,33 @@
 import pytest
-from MISalign.model.image import Image
+from MISalign.model.image import Image, MISImage, MISImageFile
 import numpy as np
 
 class TestImage():
     def test_image_init(self):
         test_img_a01=r"example\data\set_a\a_myimages01.jpg"
         test_image=Image(test_img_a01)
-        assert str(test_image)=="Image 'a_myimages01.jpg' with shape:(1200, 1600, 3)"
-    def test_image_dfe_rect(self):
-        test_img_a01=r"example\data\set_a\a_myimages01.jpg"
-        test_image=Image(test_img_a01)
-        test_dfe_arr_fp=r"example\expected_result\set_a\dfe_rectangular.npy"
-        assert np.all(test_image.dfe_arr()==np.load(test_dfe_arr_fp))
+        assert str(test_image)=="Image 'a_myimages01.jpg' with shape:(1600, 1200)"
+    # def test_image_dfe_rect(self):
+    #     test_img_a01=r"example\data\set_a\a_myimages01.jpg"
+    #     test_image=Image(test_img_a01)
+    #     test_dfe_arr_fp=r"example\expected_result\set_a\dfe_rectangular.npy"
+    #     assert np.all(test_image.dfe_arr()==np.load(test_dfe_arr_fp))
     def test_image_img_rect(self):
         test_img_a01=r"example\data\set_a\a_myimages01.jpg"
         test_image=Image(test_img_a01)
         test_img_arr_fp=r"example\expected_result\set_a\img_a01.npy"
         assert np.all(test_image.img_arr()==np.load(test_img_arr_fp))
+
+#TODO makes tests based on `test_files` not on examples.
+class TestMISImageFile():
+    def test_protocol_isinstance(self):
+        assert isinstance(MISImageFile,MISImage)
+    def test_image_init(self):
+        test_img_a01=r"example\data\set_a\a_myimages01.jpg"
+        test_image=MISImageFile(test_img_a01)
+        assert str(test_image)=="Image 'a_myimages01' with shape:(1600, 1200)"
+    def test_image_img_rect(self):
+        test_img_a01=r"example\data\set_a\a_myimages01.jpg"
+        test_image=MISImageFile(test_img_a01)
+        test_img_arr_fp=r"example\expected_result\set_a\img_a01.npy"
+        assert np.all(test_image.get_image_array()==np.load(test_img_arr_fp))
