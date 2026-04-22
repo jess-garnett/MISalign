54b50ac5b974fc118d121dc5dbaecc2e4b6489bd
Jess Garnett <jgarnett@umd.edu>
2026-04-22 16:37:58 -0400

    Reworks the hdf5 module to be more flexible with hdf5path. Overall reducing the prescribed structure and opening up the hdf5 module to work with more hdf5 structures. Associated reworking of test artifacts & tests.

--- tests/test_model_hdf5.py
+++ tests/test_model_hdf5.py
@@ -1,4 +1,4 @@
-from MISalign.model.hdf5 import MISProjectHDF5, MISImageHDF5,load_mis_project_hdf5
+from MISalign.model.hdf5 import MISProjectHDF5, MISImageHDF5
 from MISalign.model.project import MISProject
 from MISalign.model.image import MISImage, setup_image
 from pathlib import Path
@@ -8,18 +8,19 @@ class TestMISProjectHDF5():
     # def test_protocol_isinstance(self):
     #     assert isinstance(MISProjectHDF5,MISProject)
     def test_load_str(self):
-        test_filepath="tests/test_files/model_hdf5/mytestfile1.hdf5"
-        mp=load_mis_project_hdf5(test_filepath)
+        test_filepath="tests/test_files/model_hdf5/test-project_a-rel-cal-comp.hdf5"
+        project_hdf5path="MISContainer0/MISProjectJSON0"
+        mp=MISProjectHDF5.load(test_filepath,project_hdf5path)
         expected_result=f"""A MISalign project with:
 Images:
-    a_myimages01.jpg
-    a_myimages02.jpg
-    a_myimages03.jpg
+    image_a01.jpg
+    image_a02.jpg
+    image_a03.jpg
 Relations:
-    ('a_myimages01.jpg', 'a_myimages02.jpg')
-    ('a_myimages02.jpg', 'a_myimages03.jpg')
+    ('image_a01.jpg', 'image_a02.jpg')
+    ('image_a02.jpg', 'image_a03.jpg')
 Calibration:
-    pixel : 599.006361608906
+    pixel : 600
     length : 1
     length_unit : mm
 Project Path:
@@ -29,8 +30,9 @@ Project Path:
 class Test_setup_image():
     def test_setup_imagefile(self):
         assert type(setup_image(
-            image_name="a_myimages01.jpg",
-            hdf5_filepath="tests/test_files/model_hdf5/mytestfile1.hdf5",
+            image_name="image_a01.jpg",
+            hdf5_filepath="tests/test_files/model_hdf5/test-project_a-rel-cal-comp.hdf5",
+            hdf5path="/MISContainer0/MISDataContainer0/image_a01.jpg",
             image_type="hdf5",
             PIL_mode="RGB"
         ))==MISImageHDF5
@@ -40,56 +42,64 @@ class TestMISImageHDF5():
         assert isinstance(MISImageHDF5,MISImage)
     def test_image_init(self):
         test_image_data=dict(
-            image_name="a_myimages01.jpg",
-            hdf5_filepath="tests/test_files/model_hdf5/mytestfile1.hdf5",
+            image_name="image_a01.jpg",
+            hdf5_filepath="tests/test_files/model_hdf5/test-project_a-rel-cal-comp.hdf5",
+            hdf5path="/MISContainer0/MISDataContainer0/image_a01.jpg",
             image_type="hdf5",
             PIL_mode="RGB")
         test_image=MISImageHDF5(**test_image_data)
-        assert str(test_image)=="Image 'a_myimages01.jpg' with shape:(1600, 1200)"
+        assert str(test_image)=="Image 'image_a01.jpg' with shape:(1600, 1200)"
     def test_image_save(self):
         test_image_data=dict(
-            image_name="a_myimages01.jpg",
-            hdf5_filepath="tests/test_files/model_hdf5/mytestfile1.hdf5",
+            image_name="image_a01.jpg",
+            hdf5_filepath="tests/test_files/model_hdf5/test-project_a-rel-cal-comp.hdf5",
+            hdf5path="/MISContainer0/MISDataContainer0/image_a01.jpg",
             image_type="hdf5",
             PIL_mode="RGB")
         test_image=MISImageHDF5(**test_image_data)
         assert test_image.save_dict()==dict(
-            image_name="a_myimages01.jpg",
-            hdf5_filepath="tests/test_files/model_hdf5/mytestfile1.hdf5",
+            image_name="image_a01.jpg",
+            hdf5_filepath="tests/test_files/model_hdf5/test-project_a-rel-cal-comp.hdf5",
+            hdf5path="/MISContainer0/MISDataContainer0/image_a01.jpg",
             image_type="hdf5",
             PIL_mode="RGB")
     def test_image_save_note(self):
         test_image_data=dict(
-            image_name="a_myimages01.jpg",
-            hdf5_filepath="tests/test_files/model_hdf5/mytestfile1.hdf5",
+            image_name="image_a01.jpg",
+            hdf5_filepath="tests/test_files/model_hdf5/test-project_a-rel-cal-comp.hdf5",
+            hdf5path="/MISContainer0/MISDataContainer0/image_a01.jpg",
             image_type="hdf5",
             PIL_mode="RGB",
             note="Test note")
         test_image=MISImageHDF5(**test_image_data)
         assert test_image.save_dict()==dict(
-            image_name="a_myimages01.jpg",
-            hdf5_filepath="tests/test_files/model_hdf5/mytestfile1.hdf5",
+            image_name="image_a01.jpg",
+            hdf5_filepath="tests/test_files/model_hdf5/test-project_a-rel-cal-comp.hdf5",
+            hdf5path="/MISContainer0/MISDataContainer0/image_a01.jpg",
             image_type="hdf5",
             PIL_mode="RGB",
             note="Test note")
-    def test_image_save_change(self):
+    def test_image_save_change_filepath(self):
         test_image_data=dict(
-            image_name="a_myimages01.jpg",
-            hdf5_filepath="tests/test_files/model_hdf5/mytestfile1.hdf5",
+            image_name="image_a01.jpg",
+            hdf5_filepath="tests/test_files/model_hdf5/test-project_a-rel-cal-comp.hdf5",
+            hdf5path="/MISContainer0/MISDataContainer0/image_a01.jpg",
             image_type="hdf5",
             PIL_mode="RGB")
         test_image=MISImageHDF5(**test_image_data)
-        change_test_img_a01="tests/test_files/model_hdf5/mytestfile2.hdf5"
+        change_test_img_a01="tests/test_files/model_hdf5/test-project_a-rel-cal-comp-change.hdf5"
         test_image.hdf5_filepath=Path(change_test_img_a01)
         assert test_image.save_dict()==dict(
-            image_name="a_myimages01.jpg",
-            hdf5_filepath="tests/test_files/model_hdf5/mytestfile2.hdf5",
+            image_name="image_a01.jpg",
+            hdf5_filepath="tests/test_files/model_hdf5/test-project_a-rel-cal-comp-change.hdf5",
+            hdf5path="/MISContainer0/MISDataContainer0/image_a01.jpg",
             image_type="hdf5",
             PIL_mode="RGB")
     def test_image_img_rect(self):
         test_image_data=dict(
-            image_name="a_myimages01.jpg",
-            hdf5_filepath="tests/test_files/model_hdf5/mytestfile1.hdf5",
+            image_name="image_a01.jpg",
+            hdf5_filepath="tests/test_files/model_hdf5/test-project_a-rel-cal-comp.hdf5",
+            hdf5path="/MISContainer0/MISDataContainer0/image_a01.jpg",
             image_type="hdf5",
             PIL_mode="RGB")
         test_image=MISImageHDF5(**test_image_data)
