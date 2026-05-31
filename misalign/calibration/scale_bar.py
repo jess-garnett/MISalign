11b20b6b57f53bc11413f371fe69462aff409dc0
Jess Garnett <jgarnett@umd.edu>
2026-05-31 12:00:41 -0400

    Updated type hints and docstrings in scale bar.

--- misalign/calibration/scale_bar.py
+++ misalign/calibration/scale_bar.py
@@ -1,30 +1,51 @@
+"""
+Matplotlib-based Scale Bar Module
+"""
+from typing import Any
 from matplotlib import pyplot as plt
 from matplotlib import axes
 from mpl_toolkits.axes_grid1.anchored_artists import AnchoredSizeBar
 from PIL import Image as PILImage
 from quantiphy import Quantity
+from logging import warning
+from pathlib import Path
+import numpy as np
 
 def add_scale_bar(axes:axes.Axes,
                   scale_measurement:str,
-                  calibration:dict,
-                  **AnchoredSizeBar_kwargs:dict
+                  calibration:dict[str,Any],
+                  **AnchoredSizeBar_kwargs
                   ):
+    """
+    Add scale bar to matplotlib axes.
+
+    Parameters
+    ----------
+    axes : matplotlib.axes.Axes
+        Axes to add the scale bar to.
+    scale_measurement : str
+        Label for scale bar. Must be interpretable by `quantiphy`.
+    calibration : dict[str,Any]
+        calibration dictionary of form `{"pixel":number,"length":number,"length_unit":str}`
+    **AnchoredSizeBar_kwargs : kwargs
+        All other kwargs are passed to `mpl_toolkits.axes_grid1.anchored_artists.AnchoredSizeBar`
+        Defaults: `{"loc":'upper left', "pad":0.5, "borderpad":0.5, "sep":2, "frameon":True,}`
+    """
     # Generate scale
-    pixel_distance=calibration["pixel"]
+    pixel_distance:int=calibration["pixel"]
     length_quantity=Quantity(f"{calibration['length']} {calibration['length_unit']}")
     scale_measure_quantity=Quantity(scale_measurement)
     scale_ratio=scale_measure_quantity/length_quantity
     pixels_scaled=scale_ratio*pixel_distance
     # AnchoredSizeBar arg setup
-    asb_kwargs={
+    asb_kwargs_defaults={
         "loc":'upper left',
         "pad":0.5,
         "borderpad":0.5,
         "sep":2,
         "frameon":True,
     }
-    for key,value in AnchoredSizeBar_kwargs.items():
-        asb_kwargs[key]=value
+    asb_kwargs = asb_kwargs_defaults | AnchoredSizeBar_kwargs
     # Creates and add scale bar to axes
     scale_bar=AnchoredSizeBar(
         axes.transData,
@@ -33,21 +54,54 @@ def add_scale_bar(axes:axes.Axes,
         **asb_kwargs
     )
     axes.add_artist(scale_bar)
-def image_with_scale_bar(image:str,
+
+def image_with_scale_bar(image:Path|str|Any,
                   scale_measurement:str,
                   calibration:dict,
                   **AnchoredSizeBar_kwargs
                   ):
+    """
+    Show image with scale bar.
+
+    Sets up matplotlib figure and axes then shows image with scale bar.
+
+    Parameters
+    ----------
+    image : Path | str | Any
+        Either `Path` or path-like string to image file or a `matplotlib.pyplot.imshow` compatible array.
+    scale_measurement : str
+        Label for scale bar. Must be interpretable by `quantiphy`.
+    calibration : dict[str,Any]
+        calibration dictionary of form `{"pixel":number,"length":number,"length_unit":str}`
+    **AnchoredSizeBar_kwargs : kwargs
+        All other kwargs are passed to `mpl_toolkits.axes_grid1.anchored_artists.AnchoredSizeBar`
+        Defaults: `{"loc":'upper left', "pad":0.5, "borderpad":0.5, "sep":2, "frameon":True,}`
+    """
+    if not plt.isinteractive():
+        warning("Matplotlib is not running in interactive mode and rescaling will not work. Please switch to an interactive mpl backend.")
     
-    if type(image) is str:
-        image=PILImage.open(image)
+    if type(image) is str or type(image) is Path:
+        array=np.asarray(PILImage.open(image))
+    else:
+        array=np.asarray(image)
     
-    fig=plt.figure()
-    plt.imshow(image)
+    plt.figure()
+    plt.imshow(array)
     add_scale_bar(plt.gca(),scale_measurement,calibration,**AnchoredSizeBar_kwargs)
     plt.gca().set_axis_off()
     plt.show()
+
 def scale_bar_calibrate(scale_dpi:int):
+    """
+    Scale current matplotlib figure size relative to image size and desired DPI.
+
+    Increasing the DPI for a set image size makes the effective image size smaller, resulting in fonts or other features being larger.
+
+    Parameters
+    ----------
+    scale_dpi : int
+        Scaled image DPI.
+    """
     plt.tight_layout(pad=0)
     y_lim=plt.gca().get_ylim()
     y_size=abs(int(y_lim[0]-y_lim[1]))
@@ -56,5 +110,19 @@ def scale_bar_calibrate(scale_dpi:int):
     y_figsize=y_size/scale_dpi
     x_figsize=x_size/scale_dpi
     plt.gcf().set_size_inches(x_figsize,y_figsize)
-def save_calibrated_image(image_filepath:str,scale_dpi:int):
+
+def save_calibrated_image(image_filepath:Path|str,scale_dpi:int):
+    """
+    Save image with scale bar.
+
+    When used with the same `scale_dpi` as `scale_bar_calibrate` will produce a 1:1 scale image.
+    If `scale_dpi` is decreased for saving it will result in a scaled down image.
+
+    Parameters
+    ----------
+    image_filepath : Path | str
+        `Path` or path-like string for image file.
+    scale_dpi : int
+        Scaled image DPI.
+    """
     plt.savefig(image_filepath,bbox_inches="tight",pad_inches=0,dpi=scale_dpi)
