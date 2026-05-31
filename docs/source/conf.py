87c5538e4db5164dcd5ab2c57b833c1fda1fb3d3
Jess Garnett <jgarnett@umd.edu>
2026-05-31 18:27:41 -0400

    Updates RTD logo path.

--- docs/source/conf.py
+++ docs/source/conf.py
@@ -43,4 +43,4 @@ exclude_patterns = []
 # https://www.sphinx-doc.org/en/master/usage/configuration.html#options-for-html-output
 
 html_theme = 'sphinx_rtd_theme'
-html_logo = '../../develop/misalign_logo_r2_wide.png'
+html_logo = '../../develop/misalign_logo/misalign_logo_r2_wide.png'
